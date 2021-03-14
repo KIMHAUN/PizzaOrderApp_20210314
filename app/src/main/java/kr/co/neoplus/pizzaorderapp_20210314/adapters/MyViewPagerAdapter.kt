e12958d1e46d81f5e68c2558eb2afc574ee3d484
kimhaun <wha02068@gmail.com>
2021-03-14 11:15:48 +0900

    뷰페이저용 어댑터 추가

---
+++ app/src/main/java/kr/co/neoplus/pizzaorderapp_20210314/adapters/MyViewPagerAdapter.kt
@@ -0,0 +1,21 @@
+package kr.co.neoplus.pizzaorderapp_20210314.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kr.co.neoplus.pizzaorderapp_20210314.fragments.MyProfileFragment
+import kr.co.neoplus.pizzaorderapp_20210314.fragments.PizzaStoresFragment
+
+class MyViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+            0 -> PizzaStoresFragment()
+            else -> MyProfileFragment()
+
+        }
+    }
+}
