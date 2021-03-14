6598bec1333f7fcf61b618032d15344c99e8b047
kimhaun <wha02068@gmail.com>
2021-03-14 11:45:57 +0900

    가게 어댑터 뼈대작업 진행

---
+++ app/src/main/java/kr/co/neoplus/pizzaorderapp_20210314/adapters/StoreAdapter.kt
@@ -0,0 +1,30 @@
+package kr.co.neoplus.pizzaorderapp_20210314.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kr.co.neoplus.pizzaorderapp_20210314.R
+import kr.co.neoplus.pizzaorderapp_20210314.datas.Store
+
+class StoreAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList: ArrayList<Store>): ArrayAdapter<Store>(mContext,resId,mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convrtview
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.store_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
+
+}
